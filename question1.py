4bba69720a62a3a332bd3a8332c773b2054f48c6
Jonathan Bateman <jondeveloper0@gmail.com>
2022-12-02 19:31:05 -0500

    I successfully answered question 1

---
+++ question1.py
@@ -0,0 +1,33 @@
+
+def get_data(filename):
+    cal_list = []
+    with open(filename) as file:
+        # data = file.readlines()
+        # for line in data:
+        #     line.strip("\n")
+        #     print(line)
+
+        for line in file:
+            #line = line.split()
+            cal_list.append(line.strip())
+    sums = []
+    summed = 0
+    for values in cal_list:
+        if values == "":
+            sums.append(summed)
+            summed = 0
+            continue
+        else:
+            summed += int(values)
+            
+            
+    return max(sums)
+
+
+
+def main():
+    answer = get_data("big_input.txt")
+    print(answer)
+    #get_data("small_input.txt")
+
+main()
