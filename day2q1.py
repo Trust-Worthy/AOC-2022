f08ebd002a7040dea963c7b578f5e7f7faffcac7
Jonathan Bateman <jondeveloper0@gmail.com>
2022-12-16 00:47:16 -0500

    I was able to come up with a solution to day 2 problem 1

--- day2q1.py
+++ day2q1.py
@@ -13,33 +13,38 @@ def calculate_score(filename):
     plus the score for the outcome of the round 
     (0 if you lost, 3 if the round was a draw, and 6 if you won).
     """
-    
-    with open(filename) as file:
+
+    with open(filename) as file: 
         total_score = 0
         for line in file:
-            round_score = 0
+            line.split()
             line.strip()
+            
+            round_score = 0
+            
             opponent_throw = line[0]
-            my_throw = line[1]
+            my_throw = line[2]
 
             if my_throw == "X":
-                round_score + 1
+                round_score += 1
             elif my_throw == "Y":
-                round_score + 2
+                round_score += 2
             else:
-                round_score + 3
+                round_score += 3
 
             if opponent_throw == "A" and my_throw == "Z" or opponent_throw == "B" and my_throw == "X" or opponent_throw == "C" and my_throw == "Y":
                 total_score += round_score
-            elif my_throw == "X" and opponent_throw == "B" or my_throw == "Y" and opponent_throw == "A" or my_throw == "Z" and opponent_throw == "B":
-                total_score += round_score + 6 # six points for a win
-            elif my_throw == opponent_throw:
-                total_score += round_score + 3 #3 points for a draw
+            elif my_throw == "X" and opponent_throw == "C" or my_throw == "Y" and opponent_throw == "A" or my_throw == "Z" and opponent_throw == "B":
+                round_score += 6
+                total_score += round_score # six points for a win
+            else:
+                round_score += 3
+                total_score += round_score #3 points for a draw
 
         return total_score
 
 def main():
-    total_score = calculate_score("basic_test_input.txt")
+    total_score = calculate_score("rock_paper.txt")
     print(total_score)
 
 if __name__ == "__main__":
