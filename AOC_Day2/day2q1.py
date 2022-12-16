444731ed06c43f897b4987f9dc0a6cd89545e349
Jonathan Bateman <jondeveloper0@gmail.com>
2022-12-15 22:36:29 -0500

    I just added a variable that calculates the score of each round

--- AOC_Day2/day2q1.py
+++ AOC_Day2/day2q1.py
@@ -13,7 +13,22 @@ def calculate_score(filename):
     plus the score for the outcome of the round 
     (0 if you lost, 3 if the round was a draw, and 6 if you won).
     """
+    total_score = 0
     with open(filename) as file:
 
         for line in file:
+            round_score = 0
             line.strip()
+            opponent_throw = line[0]
+            my_throw = line[1]
+
+            if my_throw == "X":
+                round_score + 1
+            elif my_throw == "Y":
+                round_score + 2
+            else:
+                round_score + 3
+
+            if opponent_throw == "A" and my_throw == "Z":
+
+
