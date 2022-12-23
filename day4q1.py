4bc33431336333010d00877baf0d0599275d35cd
Jonathan Bateman <jondeveloper0@gmail.com>
2022-12-23 00:54:45 -0500

    I had to convert the ranges to integers because they were strings. Overall, on the test input I think my code works.

--- day4q1.py
+++ day4q1.py
@@ -13,16 +13,16 @@ def is_superset(filename):
             range1 = line[0]
             range2 = line[1]
             for i in range(len(range1)):
-                if line[i] == "-":
-                    start_range1 = line[:i]
-                    end_range1 = line[i+1:len(range1)]
+                if range1[i] == "-":
+                    start_range1 = int(range1[:i])
+                    end_range1 = int(range1[i+1:len(range1)])
                     range1_set = set()
                     for num in range(start_range1,end_range1 + 1):
                         range1_set.add(num)
             for i in range(len(range2)):
-                if line[i] == "-":
-                    start_range2 = line[:i]
-                    end_range2 = line[i+1:len(range2)]
+                if range2[i] == "-":
+                    start_range2 = int(range2[:i])
+                    end_range2 = int(range2[i+1:len(range2)])
                     range2_set = set()
                     for num in range(start_range2,end_range2+1):
                         range2_set.add(num)
@@ -30,15 +30,10 @@ def is_superset(filename):
                 times += 1
             if range2_set.issubset(range1_set):
                 times += 1
-
-            
-
-
-
-
+    return times
 
 def main():
-    is_superset("Test_Input/day4_test_input.txt")
+    print(is_superset("Test_Input/day4_test_input.txt"))
 
 if __name__ == "__main__":
     main()
